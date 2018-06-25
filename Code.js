0f1da550b48552ee0c484aa322c5a5295fad767b
Kristoffer Lorentsen <krilor@gmail.com>
2018-06-25 21:07:18 +0200

    Remove some comments to remove unwanted scope and fix #3

--- Code.js
+++ Code.js
@@ -1,9 +1,6 @@
 /**
  * Creates a menu entry in the Google Docs UI when the document is opened.
  *
- * @param {object} e The event parameter for a simple onOpen trigger. To
- *     determine which authorization mode (ScriptApp.AuthMode) the trigger is
- *     running in, inspect e.authMode.
  */
 
 
@@ -29,11 +26,6 @@ function onOpen(e) {
 /**
  * Runs when the add-on is installed.
  *
- * @param {object} e The event parameter for a simple onInstall trigger. To
- *     determine which authorization mode (ScriptApp.AuthMode) the trigger is
- *     running in, inspect e.authMode. (In practice, onInstall triggers always
- *     run in AuthMode.FULL, but onOpen triggers may be AuthMode.LIMITED or
- *     AuthMode.NONE.)
  */
 function onInstall(e) {
   onOpen(e);
